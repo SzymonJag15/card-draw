2de239d81d5488d146fa166875b54ddb4458b413
SzymonJag15 <69639056+SzymonJag15@users.noreply.github.com>
2022-04-26 17:56:49 +0200

    Add basic render card (#2)
    * Add basic render card
    
    * Add render index card
    
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>

--- js/cards.js
+++ js/cards.js
@@ -8,18 +8,112 @@ const getPokemon = async ({ url }) => {
   return pokemon;
 };
 
-const getCards = async () => {
-  const cards = [];
+const pickOneCard = (cards) => cards[Math.floor(Math.random() * cards.length)];
 
+const getTemplatePokeCard = (cardData) => {
+  const image = cardData.sprites.other.dream_world.front_default;
+  const hpStats = cardData.stats[0].base_stat;
+  const name = cardData.name;
+  const attackStatsValue = cardData.stats[1].base_stat;
+  const attackStatsName = cardData.stats[1].stat.name;
+  const defenceStatsValue = cardData.stats[2].base_stat;
+  const defenceStatsName = cardData.stats[2].stat.name;
+  const speedStatsValue = cardData.stats[5].base_stat;
+  const speedStatsName = cardData.stats[5].stat.name;
+
+  const card = `
+    <img 
+      class="card-item__image" 
+      src="${image}"
+      alt=""
+    >
+    <p class="card-item__hp">
+      <span class="card-item__hp--label">HP</span>
+      ${hpStats}
+    </p>
+    <div>
+      <p class="card-item__title">${name}</p>
+      <div class="card-item__stats-wrapper">
+        <div class="card-item__single-stats">
+          <p class="card-item__stats-value">${attackStatsValue}</p>
+          <p class="card-item__stats-name">${attackStatsName}</p>
+        </div>
+        <div class="card-item__single-stats">
+          <p class="card-item__stats-value">${defenceStatsValue}</p>
+          <p class="card-item__stats-name">${defenceStatsName}</p>
+        </div>
+        <div class="card-item__single-stats">
+          <p class="card-item__stats-value">${speedStatsValue}</p>
+          <p class="card-item__stats-name">${speedStatsName}</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  return card;
+};
+
+const getIndexCard = (index) => {
+  const card = `
+    <p class="card-item__title">${index}</p>
+  `;
+
+  return card;
+}
+
+const renderPokeCard = async (isVisible) => {
   const response = await fetch(`${API_URL}/pokemon?limit=${POKE_LIMIT}`);
   const { results } = await response.json();
 
-  results.map(async (pokemon) => {
-    const pokemonData = await getPokemon(pokemon);
-    cards.push(pokemonData);
-  });
+  const pickedCard = pickOneCard(results);
+  const pickerCardData = await getPokemon(pickedCard);
+  const card = getTemplatePokeCard(pickerCardData);
 
-  return cards;
+  if (isVisible) {
+    isVisible.innerHTML = card;
+    return;
+  }
+
+  const cardContainer = document.querySelector(".card-container");
+  const cardItem = document.createElement("div");
+
+  cardItem.classList.add("card-item");
+  cardItem.innerHTML = card;
+  cardContainer.appendChild(cardItem);
+}
+
+const renderIndexCard = (isVisible) => {
+  const indexArray = [...Array(10).keys()];
+  const pickedCard = pickOneCard(indexArray);
+  const pickerCardData = getIndexCard(pickedCard);
+
+  if (isVisible) {
+    isVisible.innerHTML = pickerCardData;
+    return;
+  }
+
+  const cardContainer = document.querySelector(".card-container");
+  const cardItem = document.createElement("div");
+
+  cardItem.classList.add("card-item");
+  cardItem.innerHTML = pickerCardData;
+  cardContainer.appendChild(cardItem);
+}
+
+const renderCard = async () => {
+  const isPokeMode = document.querySelector('#switch-mode__checkbox').checked;
+  const isVisible = document.querySelector(".card-item");
+
+  if(isPokeMode) {
+    renderPokeCard(isVisible);
+    return;
+  }
+
+  renderIndexCard(isVisible);
+  return;
 };
 
-getCards();
+document.addEventListener("DOMContentLoaded", () => {
+  const drawBtn = document.querySelector("#draw-card");
+  drawBtn.addEventListener("click", renderCard);
+});
